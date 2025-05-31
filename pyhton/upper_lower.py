725c9db073cf368302a0652a5fb197c051e738f2
ashish0o0 <ashishraj58707@gmail.com>
2025-05-31 15:51:51 +0530

    compare

---
+++ pyhton/upper_lower.py
@@ -0,0 +1,6 @@
+#Check if all characters in a string are lowercase or uppercase with the islower() and isupper() functions. 
+
+text = input("Enter a string: ") 
+
+print("Is the string all lowercase? ",text.islower()) 
+print("Is the string all uppercase?", text.isupper()) 
