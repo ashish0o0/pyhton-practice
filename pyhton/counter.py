0120be2f34f94110ac35406efc277feac43676f6
ashish0o0 <ashishraj58707@gmail.com>
2025-05-31 14:13:54 +0530

    python

---
+++ pyhton/counter.py
@@ -0,0 +1,13 @@
+text = input("Enter a string: ")
+
+letter_count = 0
+digit_count = 0
+
+for char in text:
+    if char.isalpha():
+        letter_count += 1
+    elif char.isdigit():
+        digit_count += 1
+    
+print("No. of letters: ", letter_count)
+print("No. of digits: ", digit_count)
