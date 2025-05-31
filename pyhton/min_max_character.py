b5ace06ec532dc82ba90ff9afb5859865cd4add7
ashish0o0 <ashishraj58707@gmail.com>
2025-05-31 15:47:18 +0530

    usese of ascii

---
+++ pyhton/min_max_character.py
@@ -0,0 +1,6 @@
+# Find the smallest and largest character in a string using the min() and max() functions. 
+
+text = input("Enter a string to find its smallest and largest characters: ") 
+
+print("Smallest character: ", min(text)) 
+print("Largest character: ", max(text))
