d6ca80076744493206303a1f53daf3cdd1a13e6f
ashish0o0 <ashishraj58707@gmail.com>
2025-05-31 15:38:44 +0530

    ASCII code

---
+++ pyhton/ascii.py
@@ -0,0 +1,4 @@
+#Determine the ASCII code of a character using the ord() function. 
+ 
+char = input("Enter a character to find its ASCII code: ")
+print("The ASCII code of", char, "is: ", ord(char))
