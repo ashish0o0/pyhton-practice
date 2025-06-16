c70b7df3cf28ba79d2d7496501c55e024b92b6ce
ashish0o0 <ashishraj58707@gmail.com>
2025-06-16 17:57:43 +0530

    duplicate remover

---
+++ geeksforgeeks/remove_duplicate.py
@@ -0,0 +1,25 @@
+def duplicateRemoved(arr):
+    new_arr = []
+
+    for num in arr:
+        if num not in new_arr:
+            new_arr.append(num)
+    
+    k = len(new_arr)
+    
+    for i in range(k):
+        arr[i] = new_arr[i]
+    print(arr)
+
+    return k
+
+arr = []
+
+n = int(input("Enter the length of the Array: "))
+for _ in range(n):
+    arr.append(int(input("Enter element: ")))
+
+is_sorted = duplicateRemoved(arr)
+
+print("Array:", arr)
+print("The new array will be:", is_sorted)
---
+++ leetcode/duplicate_remove.py
@@ -0,0 +1,19 @@
+def duplicateRemoved(arr):
+    n = len(arr)
+    new_arr = []
+
+    for num in arr:
+        if num not in new_arr:
+            new_arr.append(num)
+    return new_arr
+
+arr = []
+
+n = int(input("Enter the length of the Array: "))
+for _ in range(n):
+    arr.append(int(input("Enter element: ")))
+
+is_sorted = duplicateRemoved(arr)
+
+print("Array:", arr)
+print("The new arr will be:", is_sorted)
