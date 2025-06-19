b5ee441db984c20d17f3dd7220e0a475bd564d68
ashish0o0 <ashishraj58707@gmail.com>
2025-06-19 15:43:37 +0530

    more simplified

--- geeksforgeeks/max_cosicutive.py
+++ geeksforgeeks/max_cosicutive.py
@@ -1,34 +1,30 @@
 def maxConsicutive(arr):
     n = len(arr)
-    max = 0
-    count = 1
-    i = 0
+    max = 1
+    count_zero = 0
+    count_one = 0
     if n < 1:
         return 0
     else:
-        while i < n:
+        for i in range(n):
             if arr[i] == 1:
-                if i + 1 != n:
-                    if arr[i + 1] == 1:
-                        count += 1
-                        if max < count:
-                            max = count
-                    else:
-                        count = 1
-                        if max < count:
-                            max = count
-                else:
-                    if max < count:
-                        max = count
-            i += 1
+                count_one += 1
+                if max < count_one:
+                    max = count_one
+                count_zero = 0
+            elif arr[i] == 0:
+                count_zero += 1
+                if max < count_zero:
+                    max = count_zero
+                count_one = 0
 
-    return max
+        return max
     
 # arr = [1]
 # arr = [0]
-arr = [0, 1, 1, 0, 1, 0]
+# arr = [0, 1, 1, 0, 1, 0]
 # arr = [0, 1]
-# arr = [0, 0, 0, 0, 0]
+arr = [0, 0, 0, 0, 0]
 
 call = maxConsicutive(arr)
 
