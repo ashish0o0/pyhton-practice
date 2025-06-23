427bb57c68c32c927b4d9b5d83d1d5554bc9e50a
ashish0o0 <ashishraj58707@gmail.com>
2025-06-23 16:19:03 +0530

    complete subarray sum

--- geeksforgeeks/max_subarray.py
+++ geeksforgeeks/max_subarray.py
@@ -1,21 +1,33 @@
 def maxSub(arr):
     n = len(arr)
     sum = 0
+    s_temp = -1
+    s_i = -1
+    e_i = -1
     maximum = float('-inf')
     for i in range(n):
-        sum = sum + arr[i]
-        if maximum < sum:
-            maximum = sum
-        if sum < 0:
+        if arr[i] < 0:
             sum = 0
+            s_i = -1
+        else:
+            sum += arr[i]
 
-    return maximum
+            if s_i == -1:
+                s_i = i
+                    
+            if maximum <= sum:
+                maximum = sum
+                e_i = i
+                s_temp = s_i
 
-        
+    if s_temp == -1:
+        return [-1]
+    
+    return arr[s_temp:e_i + 1]
 
-
+#       0 1  2 3  4 5 6  7 8
 # arr = [-2,1,-3,4,-1,2,1,-5,4]
-# arr = [5,4,-1,7,8]
-arr = [1]
+arr = [-838, -329]
+# arr = [206, 20, 789, 184, 853, 948, 822, 0]
 call = maxSub(arr)
 print(call)
