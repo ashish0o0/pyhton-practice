5d372b71162789c266c96861fde7cc7127c5b6eb
ashish0o0 <ashishraj58707@gmail.com>
2025-06-24 17:51:29 +0530

    upper bound in aaray

---
+++ geeksforgeeks/upper_bound.py
@@ -0,0 +1,19 @@
+def lowerBound(arr, target):
+
+    low, high = 0, len(arr)
+
+    while low < high:
+        mid = (low + high) // 2
+        if arr[mid] <= target:
+            low = mid + 1
+        else:
+            high = mid
+
+    return low 
+
+arr = [2, 3, 7, 10, 11, 11, 25]
+# arr = [1, 1, 1, 2, 2, 3, 3, 3, 3, 3, 3, 4, 4, 4, 5, 5, 5]
+# arr = [1, 3, 6, 8, 12, 17, 23]
+target = 11
+call = lowerBound(arr, target)
+print(call)
